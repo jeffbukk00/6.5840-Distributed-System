8c88112b68fa467579c88ca56df0b41464448910
jeffbukk00 <jeffbukk00@gmail.com>
2025-10-24 16:51:03 +0900

    asynchronous role transition trial

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -8,6 +8,7 @@ package raft
 
 import (
 	//	"bytes"
+	"context"
 	"math/rand"
 	"sync"
 	"sync/atomic"
@@ -16,9 +17,19 @@ import (
 	//	"6.5840/labgob"
 	"6.5840/labrpc"
 	"6.5840/raftapi"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
+const HeartbeatInterval = 100
+const ElectionTimeoutBase = 500
+
+type Role int
+
+const (
+	Follower Role = iota
+	Candidate
+	Leader
+)
 
 // A Go object implementing a single Raft peer.
 type Raft struct {
@@ -32,16 +43,38 @@ type Raft struct {
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
 
+	currentTerm int
+	votedFor    int
+	// log[]
+
+	// commitIndex
+	// lastApplied
+	// nextIndex[]
+	// matchedIndex[]
+
+	role                   Role
+	roleVersion            int
+	updateRoleMessageQueue chan UpdateRoleMessage
+
+	resetElectionTimeoutFlagChan chan struct{}
+}
+
+type UpdateRoleMessage struct {
+	role        Role
+	roleVersion int
 }
 
 // return currentTerm and whether this server
 // believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
 
-	var term int
-	var isleader bool
+	if rf.role == Leader {
+		return rf.currentTerm, true
+	}
 	// Your code here (3A).
-	return term, isleader
+	return rf.currentTerm, false
 }
 
 // save Raft's persistent state to stable storage,
@@ -61,7 +94,6 @@ func (rf *Raft) persist() {
 	// raftstate := w.Bytes()
 	// rf.persister.Save(raftstate, nil)
 }
-
 
 // restore previously persisted state.
 func (rf *Raft) readPersist(data []byte) {
@@ -90,7 +122,6 @@ func (rf *Raft) PersistBytes() int {
 	return rf.persister.RaftStateSize()
 }
 
-
 // the service says it has created a snapshot that has
 // all info up to and including index. this means the
 // service no longer needs the log through (and including)
@@ -100,22 +131,46 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 
 }
 
-
 // example RequestVote RPC arguments structure.
 // field names must start with capital letters!
 type RequestVoteArgs struct {
 	// Your data here (3A, 3B).
+	Term         int
+	CandidiateId int
+	LastLogIndex int
+	LastlogTerm  int
 }
 
 // example RequestVote RPC reply structure.
 // field names must start with capital letters!
 type RequestVoteReply struct {
 	// Your data here (3A).
+	Term        int
+	VoteGranted bool
 }
 
 // example RequestVote RPC handler.
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (3A, 3B).
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	if rf.currentTerm > args.Term {
+		reply.Term = rf.currentTerm
+		reply.VoteGranted = false
+		return
+	}
+
+	if rf.currentTerm == args.Term && rf.votedFor == args.CandidiateId {
+		reply.Term = rf.currentTerm
+		reply.VoteGranted = true
+		return
+	}
+
+	// validation
+
+	// validation succeeded
+	rf.resetElectionTimeoutFlagChan <- struct{}{}
 }
 
 // example code to send a RequestVote RPC to a server.
@@ -145,11 +200,39 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 // capitalized all field names in structs passed over RPC, and
 // that the caller passes the address of the reply struct with &, not
 // the struct itself.
-func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
-	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
-	return ok
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId int
+
+	// PrevLogIndex int
+	// PrevLogTerm int
+	// Entries[]
+	// LeaderCommit int
 }
 
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
+
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	if rf.currentTerm > args.Term {
+		reply.Term = rf.currentTerm
+		reply.Success = false
+		return
+	} else {
+		rf.currentTerm = args.Term
+	}
+
+	reply.Term = rf.currentTerm
+	reply.Success = true
+
+	rf.resetElectionTimeoutFlagChan <- struct{}{}
+}
 
 // the service using Raft (e.g. a k/v server) wants to start
 // agreement on the next command to be appended to Raft's log. if this
@@ -170,7 +253,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 
 	// Your code here (3B).
 
-
 	return index, term, isLeader
 }
 
@@ -189,21 +271,216 @@ func (rf *Raft) Kill() {
 }
 
 func (rf *Raft) killed() bool {
+
 	z := atomic.LoadInt32(&rf.dead)
 	return z == 1
 }
 
-func (rf *Raft) ticker() {
-	for rf.killed() == false {
+func (rf *Raft) runElectionTimeout() {
+	var expired *time.Timer
 
-		// Your code here (3A)
-		// Check if a leader election should be started.
+	getRandomTimer := func() *time.Timer {
+		ms := ElectionTimeoutBase + (rand.Int63() % ElectionTimeoutBase)
 
+		return time.NewTimer(time.Duration(ms) * time.Millisecond)
+	}
 
-		// pause for a random amount of time between 50 and 350
-		// milliseconds.
-		ms := 50 + (rand.Int63() % 300)
-		time.Sleep(time.Duration(ms) * time.Millisecond)
+	expired = getRandomTimer()
+
+	// 1) reset election timeout 2) role transition by expired
+	// Leader: 1) X, 2) X
+	// Candidate: 1) X, 2) O
+	// Follower: 1) O, 2) O
+	for {
+		select {
+		case <-rf.resetElectionTimeoutFlagChan:
+			rf.mu.Lock()
+			if rf.role == Follower {
+				expired = getRandomTimer()
+			}
+			rf.mu.Unlock()
+		case <-expired.C:
+			rf.mu.Lock()
+			if rf.role != Leader {
+				rf.currentTerm++
+				rf.role = Candidate
+				rf.roleVersion++
+				rf.updateRoleMessageQueue <- UpdateRoleMessage{
+					role:        rf.role,
+					roleVersion: rf.roleVersion,
+				}
+				expired = getRandomTimer()
+			}
+			rf.mu.Unlock()
+		}
+	}
+}
+
+func (rf *Raft) runCandidateWorker(parentContext context.Context, parentWaitGroup *sync.WaitGroup, roleVersion int) {
+	args := RequestVoteArgs{}
+	reply := RequestVoteReply{}
+	// ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
+
+}
+
+type EpochByTick struct {
+	current *atomic.Int64
+}
+
+func (rf *Raft) runLeaderWorker(parentContext context.Context, parentWaitGroup *sync.WaitGroup, roleVersion int) {
+	parentWaitGroup.Add(1)
+	defer parentWaitGroup.Done()
+
+	heartbeatTicker := time.NewTicker(HeartbeatInterval * time.Millisecond)
+	defer heartbeatTicker.Stop()
+
+	epochByTick := &EpochByTick{
+		current: &atomic.Int64{},
+	}
+
+	fanoutTick := make([]chan int64, len(rf.peers))
+
+	for i, _ := range rf.peers {
+		if i != rf.me {
+			tickNotificationChan := make(chan int64)
+			fanoutTick[i] = tickNotificationChan
+
+			parentWaitGroup.Add(1)
+			go rf.runHeartbeatHandler(parentContext, parentWaitGroup, epochByTick, tickNotificationChan, roleVersion, i)
+		}
+	}
+
+	for {
+		select {
+		case <-heartbeatTicker.C:
+			current := epochByTick.current.Load()
+
+			for i, v := range fanoutTick {
+				if i != rf.me {
+					select {
+					case v <- current:
+					default:
+						// not blocked by a notification
+					}
+				}
+
+			}
+			epochByTick.current.Add(1)
+		case <-parentContext.Done():
+			return
+		}
+	}
+
+}
+
+func (rf *Raft) runHeartbeatHandler(parentContext context.Context, parentWaitGroup *sync.WaitGroup, epochByTick *EpochByTick, tickNotificationChan chan int64, roleVersion int, peer int) {
+	defer parentWaitGroup.Done()
+
+	for {
+		select {
+		case current := <-tickNotificationChan:
+			go rf.sendAppendEntries(epochByTick, current, roleVersion, peer)
+		case <-parentContext.Done():
+			// aborted by role update
+			return
+		}
+	}
+
+}
+
+func (rf *Raft) sendAppendEntries(epochByTick *EpochByTick, epochWhenWasSended int64, roleVersion int, peer int) {
+	// no synchronization with actual caller of RPC request.
+	// because, there is no mechanism serving deterministic boundary
+	//  for RPC requests in this test framework.
+
+	rf.mu.Lock()
+
+	// fence old role before sending RPC requests
+	if roleVersion < rf.roleVersion {
+		rf.mu.Unlock()
+		return
+	}
+
+	args := AppendEntriesArgs{
+		Term:     rf.currentTerm,
+		LeaderId: rf.me,
+	}
+	reply := AppendEntriesReply{}
+
+	rf.mu.Unlock()
+
+	rf.peers[peer].Call("Raft.AppendEntries", args,
+		reply)
+
+	if epochWhenWasSended < epochByTick.current.Load() {
+		return
+	}
+
+	rf.mu.Lock()
+
+	// fence old role before applying output from stale RPC requests
+	if roleVersion < rf.roleVersion {
+		rf.mu.Unlock()
+		return
+	}
+
+	if !reply.Success {
+		if rf.currentTerm < reply.Term {
+
+			// persist currentTerm
+			rf.currentTerm = reply.Term
+
+			rf.role = Follower
+			rf.roleVersion++
+			rf.updateRoleMessageQueue <- UpdateRoleMessage{
+				role:        rf.role,
+				roleVersion: rf.roleVersion,
+			}
+		}
+	}
+	rf.mu.Unlock()
+
+}
+
+func (rf *Raft) runBackgroundWorker() {
+	var currentRoleContext context.Context
+	var abortCurrentRoleContext context.CancelFunc
+	currentRoleWaitGroup := &sync.WaitGroup{}
+
+	oldRoleIsActive := false
+
+	for {
+		select {
+		case updated := <-rf.updateRoleMessageQueue:
+
+			// abort running threads with old role atomically
+			if oldRoleIsActive {
+				abortCurrentRoleContext()
+				currentRoleWaitGroup.Wait()
+				oldRoleIsActive = false
+			}
+
+			// drain old role updates
+			rf.mu.Lock()
+			if updated.roleVersion < rf.roleVersion {
+				rf.mu.Unlock()
+				break
+			}
+			rf.mu.Unlock()
+
+			// start updated role
+			currentRoleContext, abortCurrentRoleContext = context.WithCancel(context.Background())
+			oldRoleIsActive = true
+
+			switch updated.role {
+			case Follower:
+				// no background worker as Follower
+			case Candidate:
+				go rf.runCandidateWorker(currentRoleContext, currentRoleWaitGroup, updated.roleVersion)
+			case Leader:
+				go rf.runLeaderWorker(currentRoleContext, currentRoleWaitGroup, updated.roleVersion)
+			}
+		}
 	}
 }
 
@@ -229,8 +506,6 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.readPersist(persister.ReadRaftState())
 
 	// start ticker goroutine to start elections
-	go rf.ticker()
-
 
 	return rf
 }
